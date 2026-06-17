6fb70f79c2cfadd95545e629a0db5f93be9dc29b
taraji-pt <tterraquente@gmail.com>
2026-06-17 22:12:19 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,13 +1,50 @@
 function getCssVariable(name) {
+
     return getComputedStyle(
         document.documentElement
     ).getPropertyValue(name).trim();
+
 }
 
-getFlagEmoji(countryCode)
 
-formatMatchName(bet)
+function getFlagEmoji(countryCode) {
 
-formatCurrency(value)
+    if (!countryCode) {
+        return '';
+    }
 
-formatDate(date)
+    return countryCode
+        .toUpperCase()
+        .replace(/./g, char =>
+            String.fromCodePoint(
+                127397 + char.charCodeAt()
+            )
+        );
+
+}
+
+
+function formatMatchName(bet) {
+
+    return `${getFlagEmoji(bet.home_code)} ${bet.home} vs ${bet.away} ${getFlagEmoji(bet.away_code)}`;
+
+}
+
+
+function formatCurrency(value) {
+
+    return `${Number(value).toFixed(2)}€`;
+
+}
+
+
+function formatDate(date) {
+
+    if (!date) {
+        return '-';
+    }
+
+    return new Date(date)
+        .toLocaleDateString('pt-PT');
+
+}
